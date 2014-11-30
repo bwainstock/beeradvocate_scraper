b582a7918625a20fbba889444a2cfe50a47e8169
Barry Wainstock <barrywainstock@gmail.com>
2014-11-29 19:54:48 -0800

    Updated geocoder to Google.

--- ba_geo.py
+++ ba_geo.py
@@ -6,9 +6,9 @@ import argparse
 import geojson
 from geojson import Feature, FeatureCollection, Point
 import json
-from geopy.geocoders import Nominatim
+from geopy.geocoders import GoogleV3
 
-geolocator = Nominatim()
+geolocator = GoogleV3()
 
 class Bar(object):
     def __init__(self, name, rating=0, address='', lon=0, lat=0,
@@ -45,7 +45,7 @@ class Bar(object):
                 formatted_addr = (formatted_addr[:split_spot] + ' ' + formatted_addr[split_spot:])
 
             try:
-                dash = formatted_addr.index('-') 
+                dash = formatted_addr.index('-')
             except:
                 dash = ''
 
@@ -62,36 +62,37 @@ class Bar(object):
         except:
             location = ''
 
-        print truncated_address
-        print split_address
+#        print truncated_address
+#        print split_address
         if location:
-            print location.address
-            return (split_address, location.longitude, location.latitude, 
+            #print location.address
+            return (split_address, location.longitude, location.latitude,
                 location.point)
         else:
+            print split_address
             return ('None', 'None', 'None', 'None')
 
-#if __name__ == __main__:
-       
-parser = argparse.ArgumentParser(description='Accepts json file and outputs geojson')
-parser.add_argument('-i', '--input', help='The name of the input json file')
-parser.add_argument('-o', '--output', default='output.geojson',
-                    help='The name of the output file.')
-args = parser.parse_args()
+if __name__ == "__main__":
+           
+    parser = argparse.ArgumentParser(description='Accepts json file and outputs geojson')
+    parser.add_argument('-i', '--input', required=True, help='The name of the input json file')
+    parser.add_argument('-o', '--output', default='output.geojson',
+                        help='The name of the output file.')
+    args = parser.parse_args()
 
-with open(args.input, 'r') as f:
-    data = json.load(f)
+    with open(args.input, 'r') as f:
+        data = json.load(f)
 
-raw_bars = data[u'results'][u'collection1']
-bars = []
+    raw_bars = data[u'results'][u'collection1']
+    bars = []
 
-for bar in raw_bars:
-    bars.append(Bar(name=bar[u'Name'].encode('utf-8'),
-                    rating=bar[u'Rating'].encode('utf-8'),
-                   address=bar[u'Address']))
+    for bar in raw_bars:
+        bars.append(Bar(name=bar[u'Name'].encode('utf-8'),
+                        rating=bar[u'Rating'].encode('utf-8'),
+                       address=bar[u'Address']))
 
-geodata = FeatureCollection([Feature(geometry=Point((x.lon, x.lat)), id=x.name, 
-          properties={'name': x.name, 'rating': x.rating}) for x in bars if x.lon != 'None'])
+    geodata = FeatureCollection([Feature(geometry=Point((x.lon, x.lat)), id=x.name,
+              properties={'name': x.name, 'rating': x.rating}) for x in bars if x.lon != 'None'])
 
-with open(args.output, 'w') as f:
-    geojson.dump(geodata, f)
+    with open(args.output, 'w') as f:
+        geojson.dump(geodata, f)
